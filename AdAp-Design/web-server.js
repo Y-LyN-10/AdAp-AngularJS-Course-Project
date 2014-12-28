5e2bf6dd51677ac81cf1154707ff283622f132cf
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-12-28 14:25:29 +0200

    Uploaded bootstrap starter template

---
+++ AdAp-Design/web-server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+// Serve up content from public directory
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 3000, function () {
+   console.log('The server is running');
+});
